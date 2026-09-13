2439417b32b352931feea156c46c54d250bdd74f
Jyoti <jyoti1181thakur@gmail.com>
2026-09-13 20:44:07 +0530

    Add circle area calculation to variables

--- variables.c
+++ variables.c
@@ -1,11 +1,22 @@
 // write a program to calculate area of a square.
 
+//#include<stdio.h>
+//int main(){
+  //  int side;
+  //  printf("enter side of square\n");
+  // scanf("%d",&side);
+  //  printf("area of square is %d\n",side*side);
+  //  return 0;
+
+//}
+
+//write a program to calculate area of a circle.
+
 #include<stdio.h>
 int main(){
-    int side;
-    printf("enter side of square\n");
-    scanf("%d",&side);
-    printf("area of square is %d\n",side*side);
+    float radius;
+    printf("enter radius of circle\n");
+    scanf("%f",&radius);
+    printf("area of circle is %f\n",3.14*radius*radius);
     return 0;
-
 }
