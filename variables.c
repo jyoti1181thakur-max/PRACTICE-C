93fbdcf8406f74ac44dc16e8b5337d1219e850d0
Jyoti <jyoti1181thakur@gmail.com>
2026-09-13 21:16:07 +0530

    add program calculation of squre to vaaribles

---
+++ variables.c
@@ -0,0 +1,11 @@
+// WRITE A PROGRAM TO CALCULATE AREA OF A SQUARE.
+
+#include<stdio.h>
+int main(){
+    int side;
+    printf("enter side of square\n");
+    scanf("%d",&side);
+    printf("area of square is %d\n",side *side);
+    return 0;
+    
+}
