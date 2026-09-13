f8625301a581dabb61f7f5fcc626ec932ca84e53
Jyoti <jyoti1181thakur@gmail.com>
2026-09-13 20:18:15 +0530

    Add code to find area of a square to variables

---
+++ variables.c
@@ -0,0 +1,11 @@
+// write a program to calculate area of a square.
+
+#include<stdio.h>
+int main(){
+    int side;
+    printf("enter side of square\n");
+    scanf("%d",&side);
+    printf("area of square is %d\n",side*side);
+    return 0;
+
+}
